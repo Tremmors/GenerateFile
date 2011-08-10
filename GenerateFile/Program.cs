4f32e61608d7b374c041aafc4e8d2ae6b9d2db1b
johntrimis <johntrimis@gmail.com>
2011-08-10 13:04:01 -0400

    Ready to upload to github

--- GenerateFile/Program.cs
+++ GenerateFile/Program.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿// Copyright (c) 2011 John Trimis 
+//
+// MIT license:
+// Permission is hereby granted, free of charge, to any person obtaining a copy of 
+// this software and associated documentation files (the "Software"), to deal in 
+// the Software without restriction, including without limitation the rights to 
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of 
+// the Software, and to permit persons to whom the Software is furnished to do so, 
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all 
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -33,7 +53,6 @@ namespace GenerateFile
                 using ( System.IO.FileStream fileOutput = new System.IO.FileStream( OutputFileName, System.IO.FileMode.Create ) )
                 {
 
-                    Random r = new Random();
                     for ( long i = 0 ; i < size ; i = i + buffer.Length )
                     {   // Take 256byte steps
 
@@ -138,6 +157,7 @@ namespace GenerateFile
 
             }   // Scalar + Unit
             return output;
+
         }   // GetBytes
     }
 }
